ac519c9125f60d1879bdc57040716ef0fd7afe2d
DerickLFAndrade <derickspy15@gmail.com>
2023-02-03 01:05:57 -0300

    conexão mongodb

---
+++ src/main/java/com/nelioalves/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nelioalves.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.nelioalves.workshopmongo.domain.User;
+
+//@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
